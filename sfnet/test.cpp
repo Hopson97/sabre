28b1fe788e68d4b2999133fe709329bf61f45fa1
Hopson97 <mhopson@hotmail.co.uk>
2019-09-23 17:24:18 +0100

    test

--- sfnet/test.cpp
+++ sfnet/test.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include "test.h"
+
 namespace sfnet {
     void print() {
         std::cout << "Hello world\n";
---
+++ sfnet/test.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace sfnet {
+    void print();
+}
+
