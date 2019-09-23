6a6bd800d773278fd010e23d459ff906293a835c
Hopson97 <mhopson@hotmail.co.uk>
2019-09-23 17:12:16 +0100

    test

--- scripts/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ "$1" = "release" ]
-then
-    ./bin/release/sf-net 
-else
-    ./bin/debug/sf-net 
-fi
---
+++ src/sfnet/test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+namespace sfnet {
+    void print() {
+        std::cout << "Hello world\n";
+    }
+}
+
